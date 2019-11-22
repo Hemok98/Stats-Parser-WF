f8d3c82be47f90e006276d18802bf90293c31663
Hemok98 <hemok98@yandex.ru>
2019-11-22 16:44:26 +0200

    Init

---
+++ StatsParser WF/MainWindow.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace StatsParser_WF
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.OutputTextBox = new System.Windows.Forms.TextBox();
+            this.getInfoButton = new System.Windows.Forms.Button();
+            this.statsTextbox = new System.Windows.Forms.TextBox();
+            this.directoryTextbox = new System.Windows.Forms.TextBox();
+            this.directoryLabel = new System.Windows.Forms.Label();
+            this.statsLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // OutputTextBox
+            // 
+            this.OutputTextBox.Location = new System.Drawing.Point(6, 12);
+            this.OutputTextBox.Multiline = true;
+            this.OutputTextBox.Name = "OutputTextBox";
+            this.OutputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.OutputTextBox.Size = new System.Drawing.Size(566, 545);
+            this.OutputTextBox.TabIndex = 0;
+            // 
+            // getInfoButton
+            // 
+            this.getInfoButton.Location = new System.Drawing.Point(590, 72);
+            this.getInfoButton.Name = "getInfoButton";
+            this.getInfoButton.Size = new System.Drawing.Size(199, 28);
+            this.getInfoButton.TabIndex = 1;
+            this.getInfoButton.Text = "Get Info";
+            this.getInfoButton.UseVisualStyleBackColor = true;
+            this.getInfoButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // statsTextbox
+            // 
+            this.statsTextbox.Location = new System.Drawing.Point(590, 137);
+            this.statsTextbox.Multiline = true;
+            this.statsTextbox.Name = "statsTextbox";
+            this.statsTextbox.Size = new System.Drawing.Size(199, 420);
+            this.statsTextbox.TabIndex = 2;
+            this.statsTextbox.Text = global::StatsParser_WF.Properties.Settings.Default.statsSaves;
+            // 
+            // directoryTextbox
+            // 
+            this.directoryTextbox.Location = new System.Drawing.Point(590, 46);
+            this.directoryTextbox.Name = "directoryTextbox";
+            this.directoryTextbox.Size = new System.Drawing.Size(199, 20);
+            this.directoryTextbox.TabIndex = 3;
+            this.directoryTextbox.Text = "D:\\stats";
+            // 
+            // directoryLabel
+            // 
+            this.directoryLabel.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.directoryLabel.Location = new System.Drawing.Point(590, 12);
+            this.directoryLabel.Name = "directoryLabel";
+            this.directoryLabel.Size = new System.Drawing.Size(199, 31);
+            this.directoryLabel.TabIndex = 4;
+            this.directoryLabel.Text = "Direction";
+            this.directoryLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // statsLabel
+            // 
+            this.statsLabel.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.statsLabel.Location = new System.Drawing.Point(590, 103);
+            this.statsLabel.Name = "statsLabel";
+            this.statsLabel.Size = new System.Drawing.Size(199, 31);
+            this.statsLabel.TabIndex = 5;
+            this.statsLabel.Text = "Stats";
+            this.statsLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 609);
+            this.Controls.Add(this.statsLabel);
+            this.Controls.Add(this.directoryLabel);
+            this.Controls.Add(this.directoryTextbox);
+            this.Controls.Add(this.statsTextbox);
+            this.Controls.Add(this.getInfoButton);
+            this.Controls.Add(this.OutputTextBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Minecraft Stats Parser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox OutputTextBox;
+        private System.Windows.Forms.Button getInfoButton;
+        private System.Windows.Forms.TextBox statsTextbox;
+        private System.Windows.Forms.TextBox directoryTextbox;
+        private System.Windows.Forms.Label directoryLabel;
+        private System.Windows.Forms.Label statsLabel;
+    }
+}
+
---
+++ StatsParser WF/MainWindow.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StatsParser_WF
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string dirName = directoryTextbox.Text;
+            OutputTextBox.Text = "";
+
+            if (System.IO.Directory.Exists(dirName))
+            {
+
+                //OutputTextBox.Text += statsCount.ToString();
+                string str = statsTextbox.Text;
+                Properties.Settings.Default.statsSaves = statsTextbox.Text;
+                Properties.Settings.Default.Save();
+                str = str.Replace("\r\n", "\n");
+                string[] statsNames = str.Split('\n');
+                
+                //string statsMass[statsTextbox.Text.]
+
+                string[] files = System.IO.Directory.GetFiles(dirName);
+                int filesCounter = 0;
+                foreach (string fileName in files)
+                {
+                    //Console.WriteLine(fileName);
+                    string uuid = fileName.Substring(dirName.Length+1);
+                    uuid = uuid.Substring(0, uuid.Length - 5);
+
+                    string uuidWeb = "";
+                    for (int i = 0; i < uuid.Length; i++)
+                    {
+                        if (uuid[i] != '-') uuidWeb += uuid[i];
+                    }
+
+
+                    string html = null;
+
+                    try
+                    {
+                        using (System.Net.WebClient web = new System.Net.WebClient())
+                        {
+                            html = web.DownloadString("https://api.mojang.com/user/profiles/" + uuidWeb + "/names");
+                        }
+                    }
+                    catch (System.Net.WebException ex)
+                    {
+                        System.Net.HttpWebResponse httpResp = (System.Net.HttpWebResponse)ex.Response;
+                        if (ex.Status == System.Net.WebExceptionStatus.ProtocolError && httpResp.StatusCode == System.Net.HttpStatusCode.NotFound)
+                        {
+                            // Страница не найдена
+                        }
+                        else throw;
+                    }
+                    
+                    if (html != null)
+                    {
+                        html = html.Substring(html.LastIndexOf("name" )+7);
+                        //textBox1.Text += html.LastIndexOf("name").ToString();
+                        html = html.Substring(0, html.IndexOf("\""));
+                    }
+                    //в итоге в html хранится имя игрока
+                    OutputTextBox.Text += "\"" + html + "\"" + "\r\n";
+
+                    // чтение из файла
+                    using (System.IO.FileStream fstream = System.IO.File.OpenRead(fileName))
+                    {
+                        // преобразуем строку в байты
+                        byte[] array = new byte[fstream.Length];
+                        // считываем данные
+                        fstream.Read(array, 0, array.Length);
+                        // декодируем байты в строку
+                        string textFromFile = System.Text.Encoding.Default.GetString(array);
+
+                        for (int statsCounter = 0; statsCounter < statsNames.Length; statsCounter++)
+                        {
+                            OutputTextBox.Text += statsNames[statsCounter] + " - "  + findStat(textFromFile, statsNames[statsCounter])  +  "\r\n";
+                        }
+                        //OutputTextBox.Text += "sneak - " + sneak + "\r\n";
+                    }
+
+                    OutputTextBox.Text +="\r\n";
+                    filesCounter++;
+                }
+                // D:\stats
+
+            }
+        }
+
+        private string findStat(string file, string statName)
+        {
+            string stat = "0";
+            if (file.IndexOf(statName) != -1)
+            {
+                file = file.Substring(file.IndexOf(statName) + statName.Length + 2);
+                string numbers = " 0123456789";
+                int i = 0;
+                stat = "";
+                while ( numbers.IndexOf(file[i]) !=-1)
+                {                   
+                    if (file[i]!=' ') stat += file[i];
+                    i++;
+                }
+                //textBox1.Text += "!";
+            }
+            return stat;
+        }
+    }
+}
